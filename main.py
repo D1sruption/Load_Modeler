2564d609c0325b128ce80ccc340f10cf22cb92be
iIgnition <afankhauser93@gmail.com>
2021-08-10 20:47:49 -0500

    add ranges

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
+import decimal
 
 data_csv = pd.read_csv("data.csv")
 df_data = pd.DataFrame(data_csv)
@@ -20,9 +21,10 @@ for i, row in df_data.iterrows():
     load_list.append(baseline)
 
 
-def do_math(load_list):
-    plus_load_list = []
-    minus_load_list = []
+def do_math(load_list, day_list):
+    load_degradation_range = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+    temp_list_high = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    temp_list_low = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
     combined_list = []
 
     plus_1 = 1
